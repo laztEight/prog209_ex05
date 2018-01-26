a11d42d9f534329ca33b5de87716b90916e6f66d
Luann Webster <luluwebster@gmail.com>
2018-01-26 04:17:27 -0800

    Initial commit

---
+++ colorpicker.js
@@ -0,0 +1,6 @@
+
+var red=document.getElementById("red");
+var green = document.getElementById("green");
+var blue = document.getElementById("blue");
+    
+btn.addEventListener("click", clickHandler, false);
