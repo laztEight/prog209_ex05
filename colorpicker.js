f86009dafc6fcf002a1000403a02604bd85d476b
Luann Webster <luluwebster@gmail.com>
2018-01-27 06:28:58 -0800

    button
    working on getting the button to work correctly

--- colorpicker.js
+++ colorpicker.js
@@ -1,6 +1,22 @@
 
-var red=document.getElementById("red");
-var green = document.getElementById("green");
-var blue = document.getElementById("blue");
+var red = 0;
+var green = 0;
+var blue = 0;
+var showColor = document.getElementById("showColor");
+ 
+var btn = document.getElementById("btn");
     
-btn.addEventListener("click", clickHandler, false);
+btn.addEventListener("click", clickHandler(), false);
+
+//btn.onclick = clickHandler();
+
+function clickHandler(){
+    alert("this button is working");
+}
+/*function clickHandler(){
+    red = document.getElementById("red");
+    green = document.getElementById("green");
+    blue = document.getElementById("blue");
+    
+    showColor.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+}*/
