2e68b63d245bf93284384a4c7a06490eb14c166c
Luann Webster <luluwebster@gmail.com>
2018-01-27 06:57:48 -0800

    hex
    write out the equilvent hex

--- colorpicker.js
+++ colorpicker.js
@@ -13,4 +13,19 @@ function clickHandler(){
     blue = document.getElementById("blue").value;
     
     showColor.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    showColor.textContent = colorToHex(showColor.style.backgroundColor);
 }
+
+function colorToHex(color) {
+    if (color.substr(0, 1) === '#') {
+        return color;
+    }
+    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+    
+    var red = parseInt(digits[2]);
+    var green = parseInt(digits[3]);
+    var blue = parseInt(digits[4]);
+    
+    var rgb = blue | (green << 8) | (red << 16);
+    return digits[1] + 'hex #' + rgb.toString(16);
+};
