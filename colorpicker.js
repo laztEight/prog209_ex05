9d7f29a5615423c998014bb1152948a8a0c2bbbc
Luann Webster <luluwebster@gmail.com>
2018-01-28 06:43:37 -0800

    comments
    Added better comments

--- colorpicker.js
+++ colorpicker.js
@@ -1,13 +1,12 @@
-//defing varables
+//defing color varables
 var red = 0;
 var green = 0;
 var blue = 0;
-var showColor = document.getElementById("showColor");
- 
-var btn = document.getElementById("btn");
-var btnReset = document.getElementById("btnReset");
+
     
-// showing the color in the box
+// color box output and button
+var showColor = document.getElementById("showColor");
+var btn = document.getElementById("btn");
 function clickHandler(){
     red = document.getElementById("red").value;
     green = document.getElementById("green").value;
@@ -16,7 +15,6 @@ function clickHandler(){
     showColor.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
     showColor.textContent = colorToHex(showColor.style.backgroundColor);
 }
-
 function colorToHex(color) {
     if (color.substr(0, 1) === '#') {
         return color;
@@ -31,8 +29,3 @@ function colorToHex(color) {
     return digits[1] + 'hex #' + rgb.toString(16);
 }
 
-function clickReset(){
-    document.getElementById("mainForm").reset();
-    showColor.style.backgroundColor = 'rgb(255, 255, 255)';
-    showColor.textContent = "";
-}
