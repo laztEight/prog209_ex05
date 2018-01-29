80b0a3b4a66fbca660995f71da3f59818e25e3ec
Luann Webster <luluwebster@gmail.com>
2018-01-29 14:49:17 -0800

    fixed reset
    put the reset button back into the js

--- colorpicker.js
+++ colorpicker.js
@@ -1,12 +1,14 @@
-//defing color varables
+
+//defing varables
 var red = 0;
 var green = 0;
 var blue = 0;
-
-    
-// color box output and button
 var showColor = document.getElementById("showColor");
+ 
 var btn = document.getElementById("btn");
+var btnReset = document.getElementById("btnReset");
+    
+// showing the color in the box
 function clickHandler(){
     red = document.getElementById("red").value;
     green = document.getElementById("green").value;
@@ -15,6 +17,7 @@ function clickHandler(){
     showColor.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
     showColor.textContent = colorToHex(showColor.style.backgroundColor);
 }
+
 function colorToHex(color) {
     if (color.substr(0, 1) === '#') {
         return color;
@@ -29,3 +32,8 @@ function colorToHex(color) {
     return digits[1] + 'hex #' + rgb.toString(16);
 }
 
+function clickReset(){
+    document.getElementById("mainForm").reset();
+    showColor.style.backgroundColor = 'rgb(255, 255, 255)';
+    showColor.textContent = "";
+}
